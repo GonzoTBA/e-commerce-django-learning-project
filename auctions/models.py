9368e3e5e38f6a9ec9c92ad7d2e79c9d4368022a
Javier Malonda <jmalonda@gmail.com>
2021-05-14 11:48:59 +0200

    Test commit

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=512)
+    price = models.DecimalField(decimal_places=2)
+    num_bids = models.IntegerField()
+    listing_owner = models.CharField(max_length=64)
+    listing_category = models.CharField(max_length=64)
+
+class Bid():
+    pass
+
+class AuctionComment():
+    pass
