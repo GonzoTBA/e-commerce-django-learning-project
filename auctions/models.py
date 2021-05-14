baf71cfa699077844ecc5b88b9e76ce529480e6a
Javier Malonda <jmalonda@gmail.com>
2021-05-14 12:12:33 +0200

    Created models

--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.base import Model
 
 
 class User(AbstractUser):
@@ -8,13 +9,15 @@ class User(AbstractUser):
 class Listing(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=512)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     num_bids = models.IntegerField()
-    listing_owner = models.CharField(max_length=64)
+    listing_owner_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listing_owners")
     listing_category = models.CharField(max_length=64)
 
 class Bid():
-    pass
+    bid_owner_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bid_owners")
+    bid_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
-class AuctionComment():
-    pass
+class Comment():
+    comment = models.CharField(max_length=512)
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE)
